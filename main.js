00f246b9eb35f6cd39287bb2fda51791f5a9252b
wacho91 <criskol.71@gmail.com>
2025-02-18 11:19:35 -0500

    Script header section

--- main.js
+++ main.js
@@ -13,3 +13,29 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__image__content ", {
+    duration: 1000,
+    delay: 1500,
+});
