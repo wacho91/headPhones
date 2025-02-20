b142b1d762de7938dfa292db19c0f703b5dd8e37
wacho91 <criskol.71@gmail.com>
2025-02-20 11:24:03 -0500

    Script store complete

--- main.js
+++ main.js
@@ -50,3 +50,24 @@ ScrollReveal().reveal(".product__card", {
     delay: 500,
     interval: 500,
 });
+
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centerSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      depth: 250,
+      modifier: 1,
+      scale: 0.75,
+      slideShadows: false,
+      stretch: -100,
+    },
+  
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
